7fd058395b402211afc9e20e03c610cd08e3411c
Mattias <lassekongo83@users.noreply.github.com>
2020-07-30 07:48:30 +0200

    Update version.sh

--- version.sh
+++ version.sh
@@ -1,33 +1,29 @@
 #!/bin/bash
 
 version=$(cat .version)
-semver=( ${version//./ } )
+semver=(${version//./ })
 ma="${semver[0]}"
 mi="${semver[1]}"
 p="${semver[2]}"
 
 read -p "Enter a number: (1. Major | 2. Minor | 3. Patch) "  ver_type
 
-if [ $ver_type != "1" ] && [ $ver_type != "2" ] && [ $ver_type != "3" ];
-then
+if [[ $ver_type != "1" ]] && [[ $ver_type != "2" ]] && [[ $ver_type != "3" ]]; then
   echo "Invalid input"
   exit 0
 fi
 
-if [[ $ver_type == "3" ]];
-then
+if [[ $ver_type == "3" ]]; then
   echo "Patch"
-  let "p++"
-elif [[ $ver_type == "2" ]];
-then
+  ((p++))
+elif [[ $ver_type == "2" ]]; then
   echo "Minor"
-  let "mi++"
+  ((mi++))
   # reset p to 0
   p=0
-elif [[ $ver_type == "1" ]];
-then
+elif [[ $ver_type == "1" ]]; then
   echo "Major"
-  let "ma++"
+  ((ma++))
   # reset p and mi to 0
   p=0
   mi=0
