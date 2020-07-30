8420e5d304358da87f5b6c32966185183d4cc308
Mattias <lassekongo83@users.noreply.github.com>
2020-07-30 07:29:46 +0200

    Update version.sh

--- version.sh
+++ version.sh
@@ -6,25 +6,25 @@ ma="${semver[0]}"
 mi="${semver[1]}"
 p="${semver[2]}"
 
-read -p "Enter version type (major|minor|patch): "  ver_type
+read -p "Enter a number: (1. Major | 2. Minor | 3. Patch) "  ver_type
 
-if [ $ver_type != "major" ] && [ $ver_type != "minor" ] && [ $ver_type != "patch" ];
+if [ $ver_type != "1" ] && [ $ver_type != "2" ] && [ $ver_type != "3" ];
 then
   echo "Invalid input"
   exit
 fi
 
-if [[ $ver_type == "patch" ]];
+if [[ $ver_type == "3" ]];
 then
   echo "Patch"
   let "p++"
-elif [[ $ver_type == "minor" ]];
+elif [[ $ver_type == "2" ]];
 then
   echo "Minor"
   let "mi++"
   # reset p to 0
   p=0
-elif [[ $ver_type == "major" ]];
+elif [[ $ver_type == "1" ]];
 then
   echo "Major"
   let "ma++"
